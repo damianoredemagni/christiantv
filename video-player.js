e8e867d443bc82e3302d0193bf092d0c449eab61
damianoredemagni <damianoredemagni@gmail.com>
2025-02-27 11:12:47 -0500

    Autoplay Sign In Fix

--- video-player.js
+++ video-player.js
@@ -1,11 +1,13 @@
 function playVideo(area) {
   const card = area.parentElement;
   if (!card.querySelector("iframe")) {
-    const iframe = document.createElement("iframe");
-    iframe.src = `https://www.youtube.com/embed/${area.dataset.video}?autoplay=1&mute=1&controls=0`;
-    iframe.frameBorder = "0";
-    card.insertBefore(iframe, area); // Insert iframe before .area
-    card.classList.add("playing"); // Hide .info
+    card.hoverTimeout = setTimeout(() => {
+      const iframe = document.createElement("iframe");
+      iframe.src = `https://www.youtube.com/embed/${area.dataset.video}?autoplay=1&mute=1&controls=0`;
+      iframe.frameBorder = "0";
+      card.insertBefore(iframe, area);
+      card.classList.add("playing");
+    }, 500); // 500ms delay
   }
 }
 
@@ -13,8 +15,9 @@ function stopVideo(area) {
   const card = area.parentElement;
   const iframe = card.querySelector("iframe");
   if (iframe) {
-    card.removeChild(iframe); // Remove iframe only
-    card.classList.remove("playing"); // Show .info again
+    clearTimeout(card.hoverTimeout); // Cancel pending play
+    card.removeChild(iframe);
+    card.classList.remove("playing");
   }
 }
 
